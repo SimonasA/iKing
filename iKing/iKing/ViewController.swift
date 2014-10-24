3a827e1d353963b88de34d86bc4a74291a4d8329
Simonas Armonas <simonas@vinted.com>
2014-10-24 11:59:38 +0100

    Add crown on forehead

--- iKing/iKing/ViewController.swift
+++ iKing/iKing/ViewController.swift
@@ -28,12 +28,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func kingWithAddedCrown(kingsPicture: UIImage) -> UIImage {
-        let orientation: Int = kingsPicture.imageOrientation.rawValue
+        let crownImage: UIImage = UIImage(named: "crown")!
+        let crownImageView: UIImageView = UIImageView(image: crownImage)
+        let mouthPosition: CGPoint = mouthPositionForImage(kingsPicture)
+        let x: CGFloat = mouthPosition.x - (crownImageView.frame.size.width / 2.0)
+        let y: CGFloat = mouthPosition.y - crownImageView.frame.size.height
+        let faceTop = CGPointMake(x, y)
+        crownImageView.frame.origin = faceTop
+        
+        if let view = previewView {
+            view.addSubview(crownImageView)
+        }
+
+        return kingsPicture
+    }
+    
+    func mouthPositionForImage(image: UIImage) -> CGPoint {
+        var foundedMouthPoint = CGPointZero
+        
+        if let view = previewView {
+            foundedMouthPoint = CGPointMake(view.frame.size.width / 2.0, view.frame.size.height / 2.0)
+        }
+
+        let orientation: Int = image.imageOrientation.rawValue
         let options = [CIDetectorAccuracy: CIDetectorAccuracyLow, CIDetectorImageOrientation: orientation]
-        let kingsCIImage: CIImage = CIImage(CGImage: kingsPicture.CGImage, options: options)
+        let kingsCIImage: CIImage = CIImage(CGImage: image.CGImage, options: options)
         let detector: CIDetector = CIDetector(ofType:CIDetectorTypeFace, context:nil, options:options);
         let results: NSArray = detector.featuresInImage(kingsCIImage, options: NSDictionary());
-        return kingsPicture
+        for r in results {
+            let face:CIFaceFeature = r as CIFaceFeature;
+            if face.hasMouthPosition {
+                foundedMouthPoint = face.mouthPosition
+            }
+        }
+        return foundedMouthPoint
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
