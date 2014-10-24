e3056e929729e5c06b77fc294665c1ff31826008
Simonas Armonas <simonas@vinted.com>
2014-10-24 01:37:52 +0100

    first commit

---
+++ iKing/iKing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKing
+//
+//  Created by Simonas Armonas on 2014-10-24.
+//  Copyright (c) 2014 SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iKing/iKingTests/iKingTests.swift
@@ -0,0 +1,36 @@
+//
+//  iKingTests.swift
+//  iKingTests
+//
+//  Created by Simonas Armonas on 2014-10-24.
+//  Copyright (c) 2014 SA. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iKingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
